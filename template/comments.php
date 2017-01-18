f88bae718fab9a4c241b8655d7e73efe63ba6ba9
Moeini <a.moeny@gmail.com>
2017-01-18 19:27:02 +0330

    Base comment.php for themes

---
+++ template/comments.php
@@ -0,0 +1,17 @@
+<div id="reviews" class="woocommerce-Reviews">
+	<div id="comments">
+		<ol class="commentlist">
+			<?php
+			$args = array();
+
+			if ( function_exists( 'is_product' ) && is_product() ) {
+				$args = apply_filters( 'woocommerce_product_review_list_args', array( 'callback' => 'woocommerce_comments' ) );
+			}
+			?>
+			<?php wp_list_comments( $args ); ?>
+		</ol>
+	</div>
+
+	<div class="clear"></div>
+</div>
+
