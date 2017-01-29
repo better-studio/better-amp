9d6401431eb92087e2cd40a557ee5d0816da5d1e
Ali Aghdam <ali@betterstudio.com>
2017-01-29 19:21:03 +0330

    Don't show posts when search query is empty!

--- template/search.php
+++ template/search.php
@@ -5,7 +5,9 @@ better_amp_get_header();
 better_amp_get_search_form();
 
 // Show search result only when user searched!
-better_amp_template_part( 'posts-' . better_amp_page_listing() );
-better_amp_template_part( 'pagination' );
+if ( get_search_query( FALSE ) !== '' ) {
+	better_amp_template_part( 'posts-' . better_amp_page_listing() );
+	better_amp_template_part( 'pagination' );
+}
 
 better_amp_get_footer();
