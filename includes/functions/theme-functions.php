31aabb7e91ca92a7183c8fa26ebf79a857e654c8
Ali Aghdam <ali@betterstudio.com>
2017-02-15 18:30:13 +0330

    Fix: share link changed to none-AMP version link.

--- includes/functions/theme-functions.php
+++ includes/functions/theme-functions.php
@@ -2441,28 +2441,18 @@ if ( ! function_exists( 'better_amp_social_share_guss_current_page' ) ) {
 	 */
 	function better_amp_social_share_guss_current_page() {
 
-		// This function is fork of "bf_social_shares_count" function and it's better to use
-		// the main function if that was available. (IF BetterFramework was available)
-		if ( function_exists( 'bf_social_share_fetch_count' ) ) {
-			return bf_social_share_guss_current_page();
-		}
+		$page_permalink = better_amp_guess_none_amp_url();
 
 		if ( is_home() || is_front_page() ) {
-			$page_title     = get_bloginfo( 'name' );
-			$page_permalink = get_home_url();
+			$page_title = get_bloginfo( 'name' );
 		} elseif ( is_single( get_the_ID() ) && ! ( is_front_page() ) ) {
-			$page_title     = get_the_title();
-			$page_permalink = get_the_permalink();
+			$page_title = get_the_title();
 		} elseif ( is_page() ) {
-			$page_title     = get_the_title();
-			$page_permalink = get_the_permalink();
+			$page_title = get_the_title();
 		} elseif ( is_category() || is_tag() || is_tax() ) {
-			global $wp_query;
-			$page_title     = single_term_title( '', FALSE );
-			$page_permalink = get_term_link( $wp_query->get_queried_object_id() );
+			$page_title = single_term_title( '', FALSE );
 		} else {
-			$page_title     = get_bloginfo( 'name' );
-			$page_permalink = get_home_url();
+			$page_title = get_bloginfo( 'name' );
 		}
 
 		return compact( 'page_title', 'page_permalink' );
