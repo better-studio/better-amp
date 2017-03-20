45e4461392c5a788088781a77669b10f809b74bc
Ali Aghdam <ali@betterstudio.com>
2017-03-21 03:09:27 +0330

    Improvement: Android titlbar color will be same as BetterAMP theme color.
    reported here: http://community.betterstudio.com/supports/ticket/doubt-with-amp/
    
    Thanks @Antonio

--- template/header.php
+++ template/header.php
@@ -3,6 +3,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width,minimum-scale=1,maximum-scale=1,initial-scale=1">
+	<meta name="theme-color" content="<?php better_amp_get_theme_mod( 'better-amp-color-theme' ); ?>">
 
 	<?php better_amp_head() ?>
 </head>
