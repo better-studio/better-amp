c8f4d94085d52ae0704c9a3817322248fd395eb7
Ali Aghdam <ali@betterstudio.com>
2017-08-21 22:45:03 +0430

    Comments pagination should eb shown only when there is more than 1 page!

--- template/single.php
+++ template/single.php
@@ -156,16 +156,18 @@ if ( better_amp_get_theme_mod( 'better-amp-post-show-comment' ) && ( comments_op
 		</ul>
 	</div>
 
-	<div class="comments-pagination pagination">
-		<?php better_amp_comments_paginate() ?>
+	<?php
 
+	if ( get_comment_pages_count() ) { ?>
+		<div class="comments-pagination pagination">
+			<?php better_amp_comments_paginate() ?>
 
-		<span class="page-numbers">
+			<span class="page-numbers">
 			<?php printf( better_amp_translation_get( 'comment_page_numbers' ), get_query_var( 'cpage' ) ? absint( get_query_var( 'cpage' ) ) : 1, get_comment_pages_count() ); ?>
 
-	</div>
-
-	<?php
+		</div>
+		<?php
+	}
 }
 
 better_amp_show_ad_location( 'amp_post_comment_after' );
