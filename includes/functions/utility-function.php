fceedfc9533703de28fae447f17461538469c0a6
Abolfazl Moeini <a.moeny@gmail.com>
2020-02-27 15:07:54 +0330

    Code reformat.

--- includes/functions/utility-function.php
+++ includes/functions/utility-function.php
@@ -1,47 +1,52 @@
 <?php
 
-/**
- * Remove anything after question mark
- *
- * Example: betterstudio.com/?publisher=great
- * becomes: betterstudio.com/
- *
- * @param string $string
- *
- * @since 1.0.0
- *
- * @return string
- */
-function better_amp_remove_query_string( $string ) {
+if ( ! function_exists( 'better_amp_remove_query_string' ) ) {
 
-	if ( preg_match( '/([^\?]+)\?/', $string, $matches ) ) {
-		return $matches[1];
+	/**
+	 * Remove anything after question mark
+	 *
+	 * Example: betterstudio.com/?publisher=great
+	 * becomes: betterstudio.com/
+	 *
+	 * @param string $string
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return string
+	 */
+	function better_amp_remove_query_string( $string ) {
+
+		if ( preg_match( '/([^\?]+)\?/', $string, $matches ) ) {
+			return $matches[1];
+		}
+
+		return $string;
 	}
-
-	return $string;
 }
 
+if ( ! function_exists( 'better_amp_filter_attributes' ) ) {
 
-/**
- * Filter AMP element attributes
- *
- * @param array  $attributes       key-value paired attributes list
- * @param array  $valid_attributes valid attributes key
- * @param string $tag_name         optional. amp tag-name
- *
- * @since 1.0.0
- *
- * @return array filtered attributes
- */
-function better_amp_filter_attributes( $attributes, $valid_attributes, $tag_name = '' ) {
+	/**
+	 * Filter AMP element attributes
+	 *
+	 * @param array  $attributes       key-value paired attributes list
+	 * @param array  $valid_attributes valid attributes key
+	 * @param string $tag_name         optional. amp tag-name
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return array filtered attributes
+	 */
+	function better_amp_filter_attributes( $attributes, $valid_attributes, $tag_name = '' ) {
 
-	$attributes = wp_array_slice_assoc( $attributes, $valid_attributes );
+		$attributes = wp_array_slice_assoc( $attributes, $valid_attributes );
 
-	return apply_filters( 'better-amp/htmldom/filter-attributes', $attributes, $tag_name, $valid_attributes );
+		return apply_filters( 'better-amp/htmldom/filter-attributes', $attributes, $tag_name, $valid_attributes );
+	}
 }
-
 
 if ( ! function_exists( 'bf_remove_class_filter' ) ) {
+
 	/**
 	 * TODO remove this filter after adding BF to BetterAMP
 	 *
@@ -71,7 +76,7 @@ if ( ! function_exists( 'bf_remove_class_filter' ) ) {
 
 		// Check that filter actually exists first
 		if ( ! isset( $wp_filter[ $tag ] ) ) {
-			return FALSE;
+			return false;
 		}
 
 		/**
@@ -92,7 +97,7 @@ if ( ! function_exists( 'bf_remove_class_filter' ) ) {
 
 		// Exit if there aren't any callbacks for specified priority
 		if ( ! isset( $callbacks[ $priority ] ) || empty( $callbacks[ $priority ] ) ) {
-			return FALSE;
+			return false;
 		}
 
 		// Loop through each filter for the specified priority, looking for our class & method
@@ -130,11 +135,11 @@ if ( ! function_exists( 'bf_remove_class_filter' ) ) {
 					unset( $GLOBALS['merged_filters'][ $tag ] );
 				}
 
-				return TRUE;
+				return true;
 			}
 		}
 
-		return FALSE;
+		return false;
 	} // bf_remove_class_filter
 }
 
